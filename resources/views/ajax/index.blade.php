096699d16990dcaa87ea5475b006f167be1c6f8c
Paulo Vargatt <pvargatt@gmail.com>
2017-10-25 01:46:46 -0200

    get com each

--- resources/views/ajax/index.blade.php
+++ resources/views/ajax/index.blade.php
@@ -6,19 +6,22 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard
-                    <button class="btn btn-primary btn-xs pull-right" id="read-data">Carregar dados</button>
+                    <button class="btn btn-primary btn-xs pull-right" id="read-data">Carregar dados </button>
                 </div>
                 <div class="panel-body">
                     <table class="table table-bordered table-striped table-condensed">
                         <thead>
                             <tr>
-                                <th>ID</th>
+                                <th>ID </th>
                                 <th>Nome</th>
                                 <th>Sobrenome</th>
                                 <th>Endereço</th>
                                 <th>Ações</th>
                             </tr>
                         </thead>
+                        |<tbody id="info-body">
+
+                        </tbody>
                     </table>
                 </div>
             </div>
@@ -33,7 +36,26 @@
         $("#read-data").on('click', function () {
 
             $.get("{{URL::to('estudantes/read')}}", function(data){
-                console.log(data);
+
+                $('#info-body').empty()
+                $.each(data, function (i, value) {
+                    var tr =$("<tr/>");
+                        tr.append($("<td/>", {
+                            text : value.id
+                        }))
+                            .append($("<td/>", {
+                            text: value.name
+                        })).append($("<td/>", {
+                            text: value.email
+                        })).append($("<td/>", {
+                            text: value.endereco
+                        })).append($("<td/>", {
+                            html: "<a href=''><i class='icon ion-plus-circled'></i></a><a href=''> <i class='icon ion-edit'></i></a><a href=''> <i class='icon ion-close-circled'></i></a>"
+                        }));
+
+                    $('#info-body').append(tr);
+
+                });
             });
         })
     </script>
